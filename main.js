f85b52fbc3a4fcd68df345d1c13468459b1653f9
A. Emmanuel <asamoahemma25817@gmail.com>
2022-09-15 12:02:15 +0000

    updated form validation

--- main.js
+++ main.js
@@ -31,19 +31,19 @@ function actionLister() {
 
 //validate form
 function validateForm() {
+    const blankFieldMessage = "Fiedl can't be blank";
     //validating card name
     let cardHolderName = cardName.value;
-    const nameErr = document.querySelector('.nameerr');
 
-    if (cardHolderName.trim() == null || cardHolderName.trim() == "" || cardHolderName.trim() == " ") {
-        nameErr.innerHTML = "Field can't be blank"
+    if (!cardHolderName.trim()) {
+        document.querySelector('.nameerr').textContent ||= blankFieldMessage;
     } else {
         const regex = /^[a-zA-Z\s]+$/;
         if (regex.test(cardHolderName) == false) {
-            nameErr.innerHTML = "Wrong format, alphabets only"
+            document.querySelector('.nameerr').textContent ||= "Wrong format, alphabets only"
         } else {
             if (cardHolderName.length < 3) {
-                nameErr.innerHTML = "Field should be at least 3 characters"
+                document.querySelector('.nameerr').textContent ||= "Field should be at least 3 characters"
             } else {
                 nameErr.innerHTML = ""
             }
@@ -52,16 +52,15 @@ function validateForm() {
 
     //Card number validation
     let cardNumberInfo = cardNumber.value;
-    let cardErr = document.querySelector('.carderr');
-    if (cardNumberInfo.trim() == null || cardNumberInfo.trim() == "" || cardNumberInfo.trim() == " ") {
-        cardErr.innerHTML = "Field can't be blank"
+    if (!cardNumberInfo.trim()) {
+        document.querySelector('.carderr').textContent ||= blankFieldMessage;
     } else {
         const regex = /^\d{12}$/;
         if (regex.test(cardNumberInfo) == false) {
-            cardErr.innerHTML = "Wrong format, 12 digit numbers only"
+            document.querySelector('.carderr').textContent ||= "Wrong format, 12 digit numbers only"
         } else {
             if (cardName.length < 3) {
-                cardErr.innerHTML = "Field should be at least 3 characters"
+                document.querySelector('.carderr').textContent ||= "Field should be at least 3 characters"
             } else {
                 cardErr.innerHTML = ""
             }
@@ -70,26 +69,24 @@ function validateForm() {
 
     //Expire fields validation
     let monthlyExpire = expireMonth.value;
-    let monthErr = document.querySelector('.montherr');
-    if (monthlyExpire.trim() == null || monthlyExpire.trim() == "" || monthlyExpire.trim() == " ") {
-        monthErr.innerHTML = "Field can't be blank"
+    if (!monthlyExpire.trim()) {
+        document.querySelector('.montherr').textContent ||= blankFieldMessage;
     } else {
         const regex = /^[0-9\s]+$/;
         if (regex.test(monthlyExpire) == false) {
-            monthErr.innerHTML = "Wrong format, numbers only"
+            document.querySelector('.montherr').textContent ||= "Wrong format, numbers only"
         } else {
             monthErr.innerHTML = ""
         }
     }
 
     let yearlyExpire = expireYear.value;
-    let yearErr = document.querySelector('.yearerr');
-    if (yearlyExpire.trim() == null || yearlyExpire.trim() == "" || yearlyExpire.trim() == " ") {
-        yearErr.innerHTML = "Field can't be blank"
+    if (!yearlyExpire.trim()) {
+        document.querySelector('.yearerr').textContent ||= blankFieldMessage;
     } else {
         const regex = /^[0-9\s]+$/;
         if (regex.test(yearlyExpire) == false) {
-            yearErr.innerHTML = "Wrong format, numbers only"
+            document.querySelector('.yearerr').textContent ||= "Wrong format, numbers only"
         } else {
             yearErr.innerHTML = ""
         }
@@ -97,13 +94,12 @@ function validateForm() {
 
     //Cvc field validation
     let cvcExpire = cvc.value;
-    let cvcErr = document.querySelector('.cvcerr');
-    if (cvcExpire.trim() == null || cvcExpire.trim() == "" || cvcExpire.trim() == " ") {
-        cvcErr.innerHTML = "Field can't be blank"
+    if (!cvcExpire.trim()) {
+        document.querySelector('.cvcerr').textContent ||= blankFieldMessage;
     } else {
         const regex = /^[0-9\s]+$/;
         if (regex.test(cvcExpire) == false) {
-            cvcErr.innerHTML = "Wrong format, numbers only"
+            document.querySelector('.cvcerr').textContent ||= "Wrong format, numbers only"
         } else {
             cvcErr.innerHTML = ""
         }
